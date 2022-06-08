68f99f90ed65190e73821d0df03b600914a79f39
Rousseau Braga <rousseau.braga@webera.com>
2022-06-07 20:14:09 -0400

    Init my remote repo

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	//find `go` executable path
+	goExecPath, err := exec.LookPath("go")
+
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Go executable", goExecPath)
+	}
+}
